52c0c9284a36c1fb1848ff4a0ae9cc8485bda960
Alexandr <31060406+Sanier@users.noreply.github.com>
2021-12-14 19:13:45 +0300

    Add files via upload

---
+++ UnitTestService.Test/CalculatorTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UnitTestService;
+
+namespace UnitTestService.Test
+{
+    [TestFixture] 
+    public class CalculatorTest
+    {
+        [Test]
+        public void AddAlwaysReturnsExceptionValue()
+        {
+            var calculatorTest = new Calculator();
+            Assert.That(calculatorTest.Add(10,220), Is.EqualTo(230));
+        }
+
+    }
+}
